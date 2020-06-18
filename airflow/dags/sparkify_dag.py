80b272e2757bcd0d81dab4f925f7769b7ab6c49a
Maulik <maulikdave9@gmail.com>
2020-06-18 22:05:57 +0000

    DAG file added

--- airflow/dags/sparkify_dag.py
+++ airflow/dags/sparkify_dag.py
@@ -14,10 +14,11 @@ default_args = {
     'start_date': datetime(2019, 1, 12),
 }
 
-dag = DAG('udac_example_dag',
+dag = DAG('sparkify-dag',
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
-          schedule_interval='0 * * * *'
+          schedule_interval='@hourly'
+          max_active_runs= 1
         )
 
 start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
