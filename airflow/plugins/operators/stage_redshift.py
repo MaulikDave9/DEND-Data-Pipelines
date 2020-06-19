a6200fd496c1376c7451d1f4d33f464d239958e6
Maulik <maulikdave9@gmail.com>
2020-06-19 04:14:26 +0000

    begin operators stage_redshift

--- airflow/plugins/operators/stage_redshift.py
+++ airflow/plugins/operators/stage_redshift.py
@@ -4,12 +4,29 @@ from airflow.utils.decorators import apply_defaults
 
 class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
+    
+    # SQL template for JSON input format
+    json_copy_sql = """
+        COPY {}
+        FROM '{}'
+        ACCESS_KEY_ID '{}'
+        SECRET_ACCESS_KEY '{}'
+        REGION 'us-west-2'
+        JSON '{}'
+        COMPUPDATE OFF;
+    """
 
     @apply_defaults
     def __init__(self,
                  # Define your operators params (with defaults) here
                  # Example:
                  # redshift_conn_id=your-connection-name
+                 aws_conn_id = 'aws_credentials',
+                 db_conn_id  = 'redshift',
+                 table       = 'staging_table_name',
+                 data_path   = '',
+                 jsonpaths_path = 'auto',
+                 count_query    = """SELECT COUNT(*) FROM table_name;""",
                  *args, **kwargs):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
