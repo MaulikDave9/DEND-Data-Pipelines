b2f2bf5368bd8389fa0a7ff51e1996044f25ed80
Maulik <maulikdave9@gmail.com>
2020-06-27 20:20:17 +0000

    add task dependency and order

--- airflow/dags/sparkify_dag.py
+++ airflow/dags/sparkify_dag.py
@@ -125,6 +125,10 @@ end_operator = DummyOperator(
 
 # tasks dependency
 
-start_operator >> stage_events_to_redshift
-start_operator >> stage_songs_to_redshift
+start_operator >> create_tables_task >> \
+[ stage_events_to_redshift, stage_songs_to_redshift] >> \
+load_songplays_table >> [load_song_dimension_table, 
+                         load_user_dimension_table,
+                         load_artist_dimension_table,
+                         load_time_dimension_table] >> \
 run_quality_checks >> end_operator
