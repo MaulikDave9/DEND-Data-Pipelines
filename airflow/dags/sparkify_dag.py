0e1c27ec25fc1fbd2001161868f8818e5d936957
Maulik <maulikdave9@gmail.com>
2020-06-28 20:43:43 +0000

    fact table load operator

--- airflow/dags/sparkify_dag.py
+++ airflow/dags/sparkify_dag.py
@@ -88,7 +88,11 @@ stage_songs_to_redshift = StageToRedshiftOperator(
 
 load_songplays_table = LoadFactOperator(
     task_id='Load_songplays_fact_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id='redshift',
+    table='songplays',
+    query=SqlQueries.songplay_table_insert
+    
 )
 
 load_user_dimension_table = LoadDimensionOperator(
--- airflow/plugins/operators/load_fact.py
+++ airflow/plugins/operators/load_fact.py
@@ -5,18 +5,34 @@ from airflow.utils.decorators import apply_defaults
 class LoadFactOperator(BaseOperator):
 
     ui_color = '#F98866'
+    
+    insert_sql = """ INSERT INTO {{table}} {select_sql}; """
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 # Operators params (with defaults)
+                 redshift_conn_id='redshift',
+                 table='',
+                 query='',
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
-
+        self.redshift_conn_id= redshift_conn_id,
+        self.table=table
+        self.query=query
+       
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+        self.log.info('LoadFactOperator execution')
+        redshift= PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        self.log.info('Created Redshift connection')
+        
+        self.log.info('Delete rows from table {}'.format(self.table))
+        redshift.run('DELETE FROM {}'.format(self.table))
+        
+        self.log.info('INSERT INTO TABLE {}'.format(self.table))
+        sql = LoadFactOperator.insert_sql.format(
+            table=self.table,
+            select_sql=self.query)
+        
+        redshift.run(sql)
+        
