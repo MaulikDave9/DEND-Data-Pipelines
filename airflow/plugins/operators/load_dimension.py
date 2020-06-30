485589d5d141f2fa9b70ce87866416a38714ba87
maulik <maulikdave9@gmail.com>
2020-06-30 02:17:11 +0000

    Operator to handle dimension tables

--- airflow/plugins/operators/load_dimension.py
+++ airflow/plugins/operators/load_dimension.py
@@ -2,21 +2,38 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+# This will be used to build four dimension tables - users, songs, artists, time 
 class LoadDimensionOperator(BaseOperator):
 
     ui_color = '#80BD9E'
+    
+    insert_sql = """INSERT INTO {table} {select_sql};"""
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 # Operators params (with defaults)
+                 redshift_conn_id='redshift',
+                 table='',
+                 query='',
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.table            = table
+        self.query            = query
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        self.log.info('LoadDimensionOperator execution')
+        redshift= PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+        self.log.info('Delete rows from table {}'.format(self.table))
+        redshift.run('DELETE FROM {}'.format(self.table))
+        
+        self.log.info('Insert rows into table {}'.format(self.table))
+        sql = LoadDimensionOperator.insert_sql.format(
+            table=self.table,
+            select_sql=self.query
+        )
+        
+        redshift.run(sql)
+        self.log.info('{} table shall have data, please check!'.format(self.table))
